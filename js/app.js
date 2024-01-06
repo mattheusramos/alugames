34498dbdbdfe8b1e32f94566d677944900505bc6
Matheus Ramos <matheuscruzq2005@gmail.com>
2024-01-06 16:23:00 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function alterarStatus(id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector(".dashboard__item__img");
+    let botao = gameClicado.querySelector(".dashboard__item__button");
+    let contagem = 0;
+
+    if(imagem.classList.contains("dashboard__item__img--rented")){
+        imagem.classList.remove("dashboard__item__img--rented");
+        botao.classList.remove("dashboard__item__button--return");
+        botao.textContent = "Alugar";
+    }else{
+        imagem.classList.add("dashboard__item__img--rented");
+        botao.classList.add("dashboard__item__button--return");
+        botao.textContent = "Devolver";
+        contagem++;
+        console.log(contagem);
+    }
+}
